faea115b45fd323f034f799dfe9548195ddb6c59
Lindsey <lmdragun@gmail.com>
2015-05-08 14:28:28 -0400

    starting has_secure_password

---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ActiveRecord::Base
+		before_create :create_remember_token
+		before_save :normalize_fields
+
+		validates :username,
+			presence: true,
+			length: { maximum: 40 }
+
+		validates :email,
+			presence: true,
+			uniqueness: { case_sensitive: false },
+			format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i } #checks for basic email structure
+
+		validates :password,
+			length: { minimum: 8 }
+
+		has_secure_password #calls method, expects password and password confirmation
+
+	#Create a new remember token for a user
+	def User.new_remember_token
+		SecureRandom.urlsafe_base64 #giving access to bcrypt
+	end
+
+	#Hashing a token
+	def User.hash(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
+
+	private
+	#Create new session token for the user
+	def create_remember_token
+		remember_token = User.hash(User.new_remember_token)
+	end
+
+	#Normalize fields for consistency:
+	def normalize_fields
+		self.email.downcase!
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
+
+  resources :libraries
+  resources :comics
+
+  resources :users
+  resources :sessions, only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+
+  get '/signup', to: 'users#new'
+  get '/signin', to: 'sessions#new'
+  delete '/signout', to: 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
