5521a8047e5ef5707e2c7dc021d5c7fb346d4619
Lindsey <lmdragun@gmail.com>
2015-05-08 14:39:34 -0400

    finished has_secure_password

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		#Find the user by email
+		user = User.find_by(email: params[:session][:email].downcase)
+			#hash of session will contain the email
+
+			#Testing if user was found AND authenticate
+			if user && user.authenticate(params[:session][:password])
+				sign_in(user)
+				redirect_back_or root_path
+			else
+				flash[:error] = "Invalid email/password"
+				redirect_to new_session_path
+			end
+
+	end
+
+	def destroy
+  sign_out
+  redirect_to root_url
+end
+
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+before_action :require_signin, except: [:new, :create]
+
+def index
+	@users = User.all
+end
+
+def show
+end
+
+def new
+		@user = User.new
+end
+
+def create
+	@user = User.new(user_params)
+	if @user.save
+		sign_in @user
+		redirect_to @user
+	else
+		render :new
+	end
+end
+
+def edit
+end
+
+def update
+end
+
+def destroy
+end
+
+private
+	def user_params
+		params.require(:user).permit(
+			:username,
+			:email,
+			:password,
+			:password_confirmation
+			)
+	end
+
+end
