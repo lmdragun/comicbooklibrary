6fe2dfb34e1a91be96d6109a64615c5bbf4a52f7
Adam Bray <adam.bray@yale.edu>
2015-05-22 17:20:31 -0400

    forgot to save one last change

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,9 +1,10 @@
 class FollowsController < ApplicationController
   def create
+
     # maybe check that the user isn't already following that user?
     @user = User.find(params[:followed_id])
-    current_user.follow(@user)
-    if @follow.save
+    
+    if current_user.follow(@user)
       flash[:notice] = "Followed user."
       redirect_to @user
     else
